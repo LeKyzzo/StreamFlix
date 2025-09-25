6bb801447ab44a5945b0e64c5bd485ed364d047b
LeKyzzo <journiacmateo@gmail.com>
2025-09-25 16:08:24 +0200

    import1280 url

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -177,13 +177,13 @@ function setHeroContent(movie) {
       layerExtra.style.backgroundPosition = "center";
     }
   }
-    // Fond global (gradient + backdrop)
-    if (heroSection && movie.backdrop_path) {
-      const bgBackdrop = buildImageUrl(
-        movie.backdrop_path,
-        TMDB_CONFIG.TMDB_IMAGE_SIZES.BACKDROP.LARGE // supposé correspondre à ~1280
-      );
-      heroSection.style.backgroundImage = `linear-gradient(to bottom, rgba(0,0,0,0.10) 0%, rgba(0,0,0,0.40) 55%, rgba(0,0,0,0.70) 100%), url(${bgBackdrop})`;
+  // Fond global (gradient + backdrop)
+  if (heroSection && movie.backdrop_path) {
+    const bgBackdrop = buildImageUrl(
+      movie.backdrop_path,
+      TMDB_CONFIG.TMDB_IMAGE_SIZES.BACKDROP.LARGE // supposé correspondre à ~1280
+    );
+    heroSection.style.backgroundImage = `linear-gradient(to bottom, rgba(0,0,0,0.10) 0%, rgba(0,0,0,0.40) 55%, rgba(0,0,0,0.70) 100%), url(${bgBackdrop})`;
   }
 
   // Fade in
